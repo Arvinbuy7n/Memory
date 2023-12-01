7bc53e5498f7f716cabaafa9c5c69210aa2d08ad
Arvinbuyan Otgonjargal <23LP6614@G301-CC0182.local>
2023-12-01 14:50:49 +0800

    week

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,81 @@
 "use client"
+
 import Image from 'next/image'
 import { useState } from 'react';
+import styles from './components/style.module.css'
+import { images } from '../../next.config';
+
+
 
 const generateData = () => {
-  const data = [];
+  const data = [
+    {
+      text: "1",
+      src: "/pine.png"
+    },
+    {
+      text: "1",
+      src: "./pine.png"
+    },
+    {
+      text: "2",
+      src: "./pine.png"
+    },
+    {
+      text: "2",
+      src: "./pine.png"
+    },
+    {
+      text: "3",
+      src: "./pine.png"
+    },
+    {
+      text: "3",
+      src: "./pine.png"
+    },
 
-  for (let i = 0; i < 8; i++) {
-    data.push({
-      value: i
-    })
-    data.push({
-      value: i
-    })
-  }
+    {
+      text: "4",
+      src: "./pine.png"
+    },
+    {
+      text: "4",
+      src: "./pine.png"
+    },
+
+  ];
+
+
 
   return data
-}
+};
 
 export default function Home() {
-  const [data, setData] = useState(generateData())
+  const [data] = useState(generateData())
+  const [flipped, setFlipped] = useState([])
 
   return (
-    <div className='flex flex-col justify-center items-center bg-teal-300 w-full h-fit gap-5'>
-      <p className='text-3xl'>Memory</p>
-      <div className='flex bg-black w-[100px] h-[100px] text-white justify-center items-center text-7xl'> X </div>
+    <div className={styles.container}>
+      <p className={styles.font}>Memory</p>
 
-      {data.map((item) => <div className='flex'>{item.value}</div>)}
+      <div className={styles.grid}>
+        {data.map((item, index) => {
+          const isFlipped = flipped.includes(index)
+          return (
+            <div key={index} className={styles.random} >
+              <div className={styles.scope}>
+
+                {isFlipped ? <img src={item.src} /> : <div onClick={(event) => {
+                  console.log(flipped.push(event.target))
+                }} className={styles.count}>{item.text}</div>}
+
+              </div>
+            </div>
+
+          )
+        })}
+      </div>
+      <button className={styles.btn}>New Game</button>
     </div>
   )
-}
+};
